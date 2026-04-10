1d8e021a3aa1eb1a3e14ef1b7a0a0b6c9a3e19f1
Nicolas Musset <musset.nicolas@gmail.com>
2026-04-10 12:50:02 +0200

    test: add HtmlEntityInlineRenderer coverage

---
+++ tests/MarkView.Avalonia.Tests/Inlines/HtmlEntityTests.cs
@@ -0,0 +1,22 @@
+using Avalonia.Controls.Documents;
+using Avalonia.Headless.XUnit;
+using MarkView.Avalonia.Rendering;
+using Xunit;
+
+namespace MarkView.Avalonia.Tests.Inlines;
+
+public class HtmlEntityTests : RenderTestBase
+{
+    [AvaloniaTheory]
+    [InlineData("&amp;", "&")]
+    [InlineData("&lt;", "<")]
+    [InlineData("&gt;", ">")]
+    [InlineData("&quot;", "\"")]
+    public void Html_entity_is_decoded_to_character(string entity, string expected)
+    {
+        var result = Render(entity);
+        var textBlock = Assert.IsType<MarkdownSelectableTextBlock>(Assert.Single(result.Children));
+        var run = Assert.IsType<Run>(Assert.Single(textBlock.Inlines!));
+        Assert.Equal(expected, run.Text);
+    }
+}
