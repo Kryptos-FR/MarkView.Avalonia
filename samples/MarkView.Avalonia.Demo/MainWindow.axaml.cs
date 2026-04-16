d6c04123cbb1197eadbf440e607885c5ccc57eb7
Nicolas Musset <musset.nicolas@gmail.com>
2026-04-16 16:50:16 +0200

    refactor(demo): move pipeline, extensions and link handler to App global setup

--- samples/MarkView.Avalonia.Demo/MainWindow.axaml.cs
+++ samples/MarkView.Avalonia.Demo/MainWindow.axaml.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
-
 using Avalonia.Controls;
-using Markdig;
 
 namespace MarkView.Avalonia.Demo;
 
@@ -10,35 +7,6 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
-        // Combine all opt-in Markdig extensions into one pipeline
-        MarkdownView.Pipeline = new MarkdownPipelineBuilder()
-            .UseSupportedExtensions()
-            .UseFootnotes()
-            .UseAlertBlocks()
-            .UseAbbreviations()
-            .UseFigures()
-            .UseMediaLinks()
-            .Build();
-
-        MarkdownView.UseTextMateHighlighting()
-                    .UseSvg()
-                    .UseMermaid();
-
         DataContext = new MainViewModel();
-
-        MarkdownView.LinkClicked += OnLinkClicked;
-    }
-
-    private void OnLinkClicked(object? sender, MarkView.Avalonia.Rendering.LinkClickedEventArgs e)
-    {
-        try
-        {
-            Process.Start(new ProcessStartInfo(e.Url) { UseShellExecute = true });
-        }
-        catch
-        {
-            // Ignore failures to open browser
-        }
     }
 }
